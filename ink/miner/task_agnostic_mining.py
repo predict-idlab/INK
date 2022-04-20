5659f7e8aa28ebca7cf08e19951e86a9759bd9cd
bsteenwi <bram.steenwinckel@ugent.be>
2022-04-20 12:04:04 +0200

    update intbitset

--- ink/miner/task_agnostic_mining.py
+++ ink/miner/task_agnostic_mining.py
@@ -12,6 +12,7 @@ import multiprocessing as mp
 from functools import lru_cache
 from multiprocessing import Pool
 from mlxtend.frequent_patterns import association_rules
+from intbitset import intbitset
 import mlxtend
 __author__ = 'Bram Steenwinckel'
 __copyright__ = 'Copyright 2020, INK'
@@ -50,51 +51,74 @@ def __agnostic_rules(miner, X_trans):
     k_as_sub = {}
     k_as_obj = {}
     cleaned_relations = set()
+    combinations = set()
+    parts = set()
     cx = matrix.tocoo()
+
     for i, j, v in tqdm(list(zip(cx.row, cx.col, cx.data))):
         if '§' in cols[j]:
             rel, obj = cols[j].split('§')
-            if rel not in relations_ab:
-                relations_ab[rel]=set()
-                inv_relations_ab[rel]=set()
-            relations_ab[rel].add((inds[i],obj))
-            inv_relations_ab[rel].add((obj,inds[i]))
+            parts.add(inds[i])
+            parts.add(obj)
+            parts.add(rel)
 
-            if rel not in k_as_sub:
-                k_as_sub[rel] = {}
-            if inds[i] not in k_as_sub[rel]:
-                k_as_sub[rel][inds[i]] = set()
-            k_as_sub[rel][inds[i]].add(obj)
+    parts_dct = dict(zip(list(parts), range(len(parts))))
 
-            if rel not in k_as_obj:
-                k_as_obj[rel] = {}
-            if obj not in k_as_obj[rel]:
-                k_as_obj[rel][obj] = set()
-            k_as_obj[rel][obj].add(inds[i])
+    for i, j, v in tqdm(list(zip(cx.row, cx.col, cx.data))):
+        if '§' in cols[j]:
+            rel, obj = cols[j].split('§')
+            combinations.add((parts_dct[inds[i]],parts_dct[obj]))
+            combinations.add((parts_dct[obj], parts_dct[inds[i]]))
+
+    combination_dct = dict(zip(list(combinations), range(len(combinations))))
+    inv_combination_dct = {v: k for k, v in combination_dct.items()}
+    for i, j, v in tqdm(list(zip(cx.row, cx.col, cx.data))):
+        if '§' in cols[j]:
+            rel, obj = cols[j].split('§')
+            if parts_dct[rel] not in relations_ab:
+                relations_ab[parts_dct[rel]]=intbitset()
+                inv_relations_ab[parts_dct[rel]]=intbitset()
+            relations_ab[parts_dct[rel]].add(combination_dct[(parts_dct[inds[i]],parts_dct[obj])])
+            inv_relations_ab[parts_dct[rel]].add(combination_dct[(parts_dct[obj],parts_dct[inds[i]])])
+
+
+            if parts_dct[rel] not in k_as_sub:
+                k_as_sub[parts_dct[rel]] = {}
+            if parts_dct[inds[i]] not in k_as_sub[parts_dct[rel]]:
+                k_as_sub[parts_dct[rel]][parts_dct[inds[i]]] = intbitset()
+            k_as_sub[parts_dct[rel]][parts_dct[inds[i]]].add(parts_dct[obj])
+
+            if parts_dct[rel] not in k_as_obj:
+                k_as_obj[parts_dct[rel]] = {}
+            if parts_dct[obj] not in k_as_obj[parts_dct[rel]]:
+                k_as_obj[parts_dct[rel]][parts_dct[obj]] = intbitset()
+            k_as_obj[parts_dct[rel]][parts_dct[obj]].add(parts_dct[inds[i]])
         else:
             cleaned_relations.add(cols[j])
 
     matrix, inds, cols = None, None, None
     gc.collect()
 
-    cleaned_relations = [c for c in cleaned_relations if len(relations_ab[c])>=miner.support]
+
+
+    cleaned_relations = [c for c in cleaned_relations if len(relations_ab[parts_dct[c]])>=miner.support]
 
     for c in cleaned_relations:
-        filter_items[('?a '+c+' ?b',)] = len(relations_ab[c])
-        filter_items[('?b ' + c + ' ?a',)] = len(relations_ab[c])
+        filter_items[('?a '+c+' ?b',)] = len(relations_ab[parts_dct[c]])
+        filter_items[('?b ' + c + ' ?a',)] = len(relations_ab[parts_dct[c]])
 
     _pr_comb = list(itertools.combinations_with_replacement(cleaned_relations,2))
 
 
     cleaned_relations = [c for c in cleaned_relations if
-                         len(relations_ab[c]) >= miner.support and c.count(':') < miner.max_rule_set - 1]
+                         len(relations_ab[parts_dct[c]]) >= miner.support and c.count(':') < miner.max_rule_set - 1]
     cleaned_single_rel = [c for c in cleaned_relations if c.count(':') == 1]
 
     _pr = list(itertools.product(cleaned_relations, repeat=2))
 
     if miner.rule_complexity > 0:
         with Pool(mp.cpu_count() - 1, initializer=__init,
-                  initargs=(k_as_sub, k_as_obj, relations_ab,inv_relations_ab, miner.max_rule_set, miner.support, cleaned_single_rel)) as pool:
+                  initargs=(k_as_sub, k_as_obj, relations_ab,inv_relations_ab, miner.max_rule_set, miner.support, cleaned_single_rel, parts_dct, combination_dct,inv_combination_dct)) as pool:
 
             for r in tqdm(pool.imap_unordered(exec_f1, _pr_comb, chunksize=1000), total=len(_pr_comb)):
                 for el in r:
@@ -120,28 +144,28 @@ def __agnostic_rules(miner, X_trans):
     rules = association_rules(df, metric="support", min_threshold=miner.support)
     miner.rules = rules
 
-def __init(d1, d2, d3, d4, d5, d6, d7):
-    global k_as_sub, k_as_obj, relations_ab, inv_relations_ab, rule_len,support,cleaned_relations
-    k_as_sub, k_as_obj, relations_ab,inv_relations_ab, rule_len,support, cleaned_relations = d1,d2,d3,d4,d5,d6,d7
+def __init(d1, d2, d3, d4, d5, d6, d7, d8,d9,d10):
+    global k_as_sub, k_as_obj, relations_ab, inv_relations_ab, rule_len,support,cleaned_relations,parts_dct,combination_dct,inv_combination_dct
+    k_as_sub, k_as_obj, relations_ab,inv_relations_ab, rule_len,support, cleaned_relations, parts_dct,combination_dct,inv_combination_dct = d1,d2,d3,d4,d5,d6,d7,d8,d9,d10
 
 def exec_f1(p):
     filter_items = {}
     if p[0].count(':') + p[1].count(':') <= rule_len:
         if p[0] != p[1]:
-            d = relations_ab[p[1]].intersection(relations_ab[p[0]])
+            d = relations_ab[parts_dct[p[1]]].intersection(relations_ab[parts_dct[p[0]]])
             if len(d) >= support:
                 filter_items[('?a ' + p[0] + ' ?b', '?a ' + p[1] + ' ?b',)] = len(d)
 
                 for c in cleaned_relations:
                     if c != p[0] and c != p[1]:
                         if p[0].count(':') + p[1].count(':') + c.count(':') <= rule_len:
-                            dd = d.intersection(relations_ab[c])
+                            dd = d.intersection(relations_ab[parts_dct[c]])
                             if len(dd) >= support:
                                 filter_items[(('?a ' + p[0] + ' ?b', '?a ' + p[1] + ' ?b'),)] = len(d)
                                 filter_items[(('?a ' + p[0] + ' ?b', '?a ' + p[1] + ' ?b'), '?a ' + c + ' ?b')] = len(
                                     dd)
 
-        d = len(inv_relations_ab[p[1]].intersection(relations_ab[p[0]]))
+        d = len(inv_relations_ab[parts_dct[p[1]]].intersection(relations_ab[parts_dct[p[0]]]))
         if d >= support:
             filter_items[('?b ' + p[0] + ' ?a', '?a ' + p[1] + ' ?b',)] = d
     return filter_items
@@ -154,41 +178,41 @@ def exec(p):
 
     if p[0].count(':') + p[1].count(':') <= rule_len - 1:
     #
-        d1 = set(k_as_sub[p[0]].keys()).intersection(set(k_as_sub[p[1]].keys()))
-        ant_subs_upper = sum([len(k_as_sub[p[0]][d]) * len(k_as_sub[p[1]][d]) for d in d1])
+        d1 = set(k_as_sub[parts_dct[p[0]]].keys()).intersection(set(k_as_sub[parts_dct[p[1]]].keys()))
+        ant_subs_upper = sum([len(k_as_sub[parts_dct[p[0]]][d]) * len(k_as_sub[parts_dct[p[1]]][d]) for d in d1])
 
-        d2 = set(k_as_obj[p[0]].keys()).intersection(set(k_as_obj[p[1]].keys()))
-        ant_objs_upper = sum([len(k_as_obj[p[0]][d]) * len(k_as_obj[p[1]][d]) for d in d2])
+        d2 = set(k_as_obj[parts_dct[p[0]]].keys()).intersection(set(k_as_obj[parts_dct[p[1]]].keys()))
+        ant_objs_upper = sum([len(k_as_obj[parts_dct[p[0]]][d]) * len(k_as_obj[parts_dct[p[1]]][d]) for d in d2])
 
         if ant_subs_upper >= support:
             for p3 in cleaned_relations:
-                all_coms = relations_ab[p3]
-                rel1_k = {k for obj in all_coms if obj[0] in k_as_obj[p[0]] for k in k_as_obj[p[0]][obj[0]]}
+                all_coms = relations_ab[parts_dct[p3]]
+                rel1_k = {k for obj in all_coms if inv_combination_dct[obj][0] in k_as_obj[parts_dct[p[0]]] for k in k_as_obj[parts_dct[p[0]]][inv_combination_dct[obj][0]]}
                 if p[0]!=p[1]:
-                    rel2_k = {k for obj in all_coms if obj[1] in k_as_obj[p[1]] for k in k_as_obj[p[1]][obj[1]]}
+                    rel2_k = {k for obj in all_coms if inv_combination_dct[obj][1] in k_as_obj[parts_dct[p[1]]] for k in k_as_obj[parts_dct[p[1]]][inv_combination_dct[obj][1]]}
                     dd = rel1_k.intersection(rel2_k)
                 else:
                     dd = rel1_k
-                zz = len({(x,y) for d in dd for x in k_as_sub[p[0]][d] for y in k_as_sub[p[1]][d] if (x,y) in all_coms})
+                zz = len({(x,y) for d in dd for x in k_as_sub[parts_dct[p[0]]][d] for y in k_as_sub[parts_dct[p[1]]][d] if (x,y) in combination_dct and combination_dct[(x,y)] in all_coms})
                 if zz>=support:
                     if ant_subs==-1:
-                        ant_subs = len({(x, y) for d in d1 for x in k_as_sub[p[0]][d] for y in k_as_sub[p[1]][d]})
+                        ant_subs = len({(x, y) for d in d1 for x in k_as_sub[parts_dct[p[0]]][d] for y in k_as_sub[parts_dct[p[1]]][d]})
                     if  ant_subs>=support:
                         cons_sub[p3] = zz
 
         if ant_objs_upper >= support:
             for p3 in cleaned_relations:
-                all_coms = relations_ab[p3]
-                rel1_k = {k for obj in all_coms if obj[0] in k_as_sub[p[0]] for k in k_as_sub[p[0]][obj[0]]}
+                all_coms = relations_ab[parts_dct[p3]]
+                rel1_k = {k for obj in all_coms if inv_combination_dct[obj][0] in k_as_sub[parts_dct[p[0]]] for k in k_as_sub[parts_dct[p[0]]][inv_combination_dct[obj][0]]}
                 if p[0] != p[1]:
-                    rel2_k = {k for obj in all_coms if obj[1] in k_as_sub[p[1]] for k in k_as_sub[p[1]][obj[1]]}
+                    rel2_k = {k for obj in all_coms if inv_combination_dct[obj][1] in k_as_sub[parts_dct[p[1]]] for k in k_as_sub[parts_dct[p[1]]][inv_combination_dct[obj][1]]}
                     dd = rel1_k.intersection(rel2_k)
                 else:
                     dd = rel1_k
-                zz = len({(x, y) for d in dd for x in k_as_obj[p[0]][d] for y in k_as_obj[p[1]][d] if (x, y) in all_coms})
+                zz = len({(x, y) for d in dd for x in k_as_obj[parts_dct[p[0]]][d] for y in k_as_obj[parts_dct[p[1]]][d] if (x,y) in combination_dct and combination_dct[(x, y)] in all_coms})
                 if zz >= support:
                     if ant_objs==-1:
-                        ant_objs = len({(x, y) for d in d2 for x in k_as_obj[p[0]][d] for y in k_as_obj[p[1]][d]})
+                        ant_objs = len({(x, y) for d in d2 for x in k_as_obj[parts_dct[p[0]]][d] for y in k_as_obj[parts_dct[p[1]]][d]})
                     if ant_objs>=support:
                         cons_objs[p3] = zz
 
